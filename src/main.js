c7e5070b168dd62a791d3da5ea5ee99fc2d850fa
Sophia Mersmann <sophia.mersmann1@gmail.com>
2020-09-27 16:43:13 +0100

    Move css files to assets/style folder

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue';
 import App from './App.vue';
 
 import 'normalize.css';
-import '@/assets/base.css';
+import '@/assets/style/base.css';
 
 createApp(App).mount('#app');

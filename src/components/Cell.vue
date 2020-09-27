62ec91f9d45d9101259d307de03e6d4e642cc66c
Sophia Mersmann <sophia.mersmann1@gmail.com>
2020-09-27 16:45:36 +0100

    Add Cell component

---
+++ src/components/Cell.vue
@@ -0,0 +1,22 @@
+<template>
+  <button class="cell">1</button>
+</template>
+
+<script>
+export default {
+  name: 'Cell',
+};
+</script>
+
+<style scoped>
+.cell {
+  --size: 1.5rem;
+
+  font-size: 0.8rem;
+  width: var(--size);
+  height: var(--size);
+  line-height: var(--size);
+  text-align: center;
+  border: 0;
+}
+</style>
